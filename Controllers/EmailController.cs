900ce931f66e87127e223ea8f93cfc720d4f6d50
szarlih <cfa@szarlih.com>
2025-02-02 18:59:28 +0100

    Initial commit

---
+++ Controllers/EmailController.cs
@@ -0,0 +1,30 @@
+﻿using EmailInvoiceExctractor;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CentralFinanceApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EmailController : ControllerBase
+    {
+        private IActiveAccounts _scrapperService;
+
+        public EmailController(IActiveAccounts scrapperService)
+        {
+            _scrapperService = scrapperService;
+        }
+
+        [HttpGet(Name = "GetEmailMessagesCount")]
+        public int EmailMessages()
+        {
+            _scrapperService.TriggerImmediateCheck();
+            return _scrapperService.GetProcessedEmailCount();
+        }
+
+        [HttpPatch(Name = "StartAccountsScrapping")]
+        public void StartScrapping()
+        {
+            
+        }
+    }
+}
